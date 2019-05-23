712baafa3a8a523164c9b931f857293f5bc873b5
Charles <charlieander@gmail.com>
2019-05-23 14:36:01 -0400

    changed quote_all functionality

--- numpy2tsv.py
+++ numpy2tsv.py
@@ -14,15 +14,14 @@
 
 
 class ConvertNumpy:
-    def __init__(self, array, labels=None, quote_all=False):
+    def __init__(self, array, labels=None):
         """
         :param array: Numpy array of shape (num_samples, *n, data) where n is any number of dimensions.
         :param labels: A list of labels (one dimension only, will update later)
-        :param quote_all: (bool) whether to quote each value
         """
         self.array = array
         self.labels = labels
-        self.quote_all = quote_all
+        self.quote_all = False
         self.num_dims = len(self.array.shape)
         self.dim_lens = [self.array.shape[x] for x in range(self.num_dims)]
         self.num_labels = len(self.labels)
@@ -51,40 +50,50 @@ class ConvertNumpy:
             fp.write(data)
 
     def _labels_to(self, filepath):
+        if self.quote_all:
+            self.labels = ['"' + x + '"' for x in self.labels]
         data = self._join_generic_sep(self.labels, "\n")
         with open(filepath, "w") as fp:
             fp.write(data)
 
-    def to_tsv(self, filepath, delim="\t"):
+    def to_tsv(self, filepath, delim="\t", quote_all=False):
         """
         :param filepath: filepath to location to save .tsv file
         :param delim: delimiter to be used in output file, default '\t'
+        :param quote_all: (bool) whether to quote each value
         :return: None
         """
         assert filepath.endswith(".tsv")
+        self.quote_all = quote_all
         self._array_to(filepath, delim)
 
-    def to_csv(self, filepath, delim=","):
+    def to_csv(self, filepath, delim=",", quote_all=False):
         """
          :param filepath: filepath to location to save .csv file
          :param delim: delimiter to be used in output file, default ','
+         :param quote_all: (bool) whether to quote each value
          :return: None
          """
         assert filepath.endswith(".csv")
+        self.quote_all = quote_all
         self._array_to(filepath, delim)
 
-    def labels_to_tsv(self, filepath):
+    def labels_to_tsv(self, filepath, quote_all=False):
         """
          :param filepath: filepath to location to save .tsv file
+         :param quote_all: (bool) whether to quote each value
          :return: None
          """
         assert filepath.endswith(".tsv")
+        self.quote_all = quote_all
         self._labels_to(filepath)
 
-    def labels_to_csv(self, filepath):
+    def labels_to_csv(self, filepath, quote_all=False):
         """
          :param filepath: filepath to location to save .csv file
+         :param quote_all: (bool) whether to quote each value
          :return: None
          """
         assert filepath.endswith(".csv")
+        self.quote_all = quote_all
         self._labels_to(filepath)
